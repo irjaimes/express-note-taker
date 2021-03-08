c9624409a64e866ca08fd8300dfd43044ede1c72
Idzel Jaimes <irjaimes@outlook.com>
2021-03-07 23:09:36 -0800

    created index.js, updated routes files

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,64 +1,42 @@
 const app = require("express").app;
 const db = require("../db/db.json");
-
+const {createNote, validateNote} = require("../index")
 const fs = require("fs");
-// use uuid to assing id to notes
-const uuid = require("uuid/v4");
-
 
 // GET request
 app.get("/api/notes", function (req, res) {
     res.JSON(db);
 });
-// GET request
-app.get("/notes", function (req, res) {
-    res.JSON(db);
+
+// POST request 
+app.post("/api/notes", (req, res) => {
+    req.body.id = db.length.toString();
+
+    if (!validateNote(req.body)) {
+        res.status(400).send('Please enter valid note title and text description!');
+    }
+    else {
+        const newNote = createNote(req.body, notes)
+        res.json(newNote)
+    }
 });
 
-// POST request using uuid to assign note an id
-app.post("/api/notes", function (req, res) {
+// DELETE request
+app.delete("/api/notes/:id", (req, res) => {
 
-    let noteId = uuid();
-    let newNote = {
-        id: noteId,
-        title: req.body.title,
-        text: req.body.text
-    };
+    let noteId = req.params.id;
 
-    // Use fs to pull data from db.json
     fs.readFile("./db/db.json", "utf8", (err, data) => {
         if (err) throw err;
 
-        // Set variable for pulled data
         const allNotes = JSON.parse(data);
-        // Push newNote to allNotes 
-        allNotes.push(newNote);
+        // Set allNewNotes to hold updated notes
+        const allNewNotes = allNotes.filter(note => note.id != noteId);
 
-        // Use fs to write new note 
-        fs.writeFile("./db/db.json", JSON.stringify(allNotes, null, 2), err => {
+        fs.writeFile("./db/db.json", JSON.stringify(allNewNotes, null, 2), err => {
             if (err) throw err;
             res.send(db);
-            console.log("New note created!")
-        });
-    });
-
-    // DELETE request
-    app.delete("/api/notes/:id", (req, res) => {
-
-        let noteId = req.params.id;
-
-        fs.readFile("./db/db.json", "utf8", (err, data) => {
-            if (err) throw err;
-
-            const allNotes = JSON.parse(data);
-            // Set newAllNotes to hold updated notes
-            const newAllNotes = allNotes.filter(note => note.id != noteId);
-
-            fs.writeFile("./db/db.json", JSON.stringify(newAllNotes, null, 2), err => {
-                if (err) throw err;
-                res.send(db);
-                console.log("The note was deleted!")
-            });
+            console.log("The note was deleted!")
         });
     });
 });
--- server.js
+++ server.js
@@ -13,6 +13,6 @@ app.use(express.static('public'));
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
 });
